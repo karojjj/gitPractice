4ce14733fb7f6e931e1097f9e018bc358a74dfb1
karojjj <karojjj@gmail.com>
2022-02-17 13:51:21 -0500

    the commit was easy

--- src/git/Batch12.java
+++ src/git/Batch12.java
@@ -6,6 +6,8 @@ public class Batch12 {
 		// TODO Auto-generated method stub
   System.out.println("we we are were");
   System.out.println("aaaaaaaaaaaaa");
+  
+  System.out.println("yes ");
 	}
 
 }
